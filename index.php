11b605a85e230fe9de675d4467dbbc59b31ae1c5
hugol071122 <vichugrhcam010710@gmail.com>
2026-03-23 20:12:18 -0600

    Practica 1

--- index.php
+++ index.php
@@ -2,18 +2,13 @@
 $nombre = "Hugo";
 $edad = 23;
 $pais = "México";
-$ciudad = "Estado de mexico";
-$profesion = " Estudiante de Ingenieria en sistemas";
-$hobby = "Jugar videojuegos";
+$ciudad = "Estado de México";
+$profesion = "Estudiante de Ingeniería en Sistemas";
+$hobby = "jugar videojuegos";
 $lenguaje_favorito = "PHP";
 $años_estudio = 4;
 $estado_laboral = "buscando nuevos retos";
 $mascota = "un perro";
 
-echo "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años. ";
-echo "Originalmente soy de " . $pais . ", específicamente del estado de " . $ciudad . ", ";
-echo "donde me desempeño como " . $profesion . ". ";
-echo "Cuento con " . $años_estudio . " años estudiando la carrera y mi lenguaje de programación favorito es " . $lenguaje_favorito . ". ";
-echo "En mis tiempos libres disfruto mucho de " . $hobby . " y de pasar tiempo con mi mascota, que es " . $mascota . ". ";
-echo "Actualmente me encuentro " . $estado_laboral . ".";
+echo "Hola, mi nombre es $nombre y tengo $edad años. Originalmente soy de $pais, específicamente del $ciudad, donde me desempeño como $profesion. Cuento con $años_estudio años estudiando la carrera y mi lenguaje de programación favorito es $lenguaje_favorito. En mis tiempos libres disfruto mucho de $hobby y de pasar tiempo con mi mascota, que es $mascota. Actualmente me encuentro $estado_laboral.";
 ?>
