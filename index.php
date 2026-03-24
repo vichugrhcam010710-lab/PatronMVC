80da67db3b7f501da605db913291b6fdda2e7fcc
hugol071122 <vichugrhcam010710@gmail.com>
2026-03-23 20:02:07 -0600

    Practica 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$nombre = "Hugo";
+$edad = 23;
+$pais = "México";
+$ciudad = "Estado de mexico";
+$profesion = " Estudiante de Ingenieria en sistemas";
+$hobby = "Jugar videojuegos";
+$lenguaje_favorito = "PHP";
+$años_estudio = 4;
+$estado_laboral = "buscando nuevos retos";
+$mascota = "un perro";
+
+echo "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años. ";
+echo "Originalmente soy de " . $pais . ", específicamente del estado de " . $ciudad . ", ";
+echo "donde me desempeño como " . $profesion . ". ";
+echo "Cuento con " . $años_estudio . " años estudiando la carrera y mi lenguaje de programación favorito es " . $lenguaje_favorito . ". ";
+echo "En mis tiempos libres disfruto mucho de " . $hobby . " y de pasar tiempo con mi mascota, que es " . $mascota . ". ";
+echo "Actualmente me encuentro " . $estado_laboral . ".";
+?>
