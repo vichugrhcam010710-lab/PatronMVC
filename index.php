7c1167cbd8999ce344ab1ee23e40511b7721ef99
hugol071122 <vichugrhcam010710@gmail.com>
2026-03-23 20:14:35 -0600

    Practica 1

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+### Mi Presentación en PHP
+
+```php
 <?php
 $nombre = "Hugo";
 $edad = 23;
@@ -10,5 +13,12 @@ $años_estudio = 4;
 $estado_laboral = "buscando nuevos retos";
 $mascota = "un perro";
 
-echo "Hola, mi nombre es $nombre y tengo $edad años. Originalmente soy de $pais, específicamente del $ciudad, donde me desempeño como $profesion. Cuento con $años_estudio años estudiando la carrera y mi lenguaje de programación favorito es $lenguaje_favorito. En mis tiempos libres disfruto mucho de $hobby y de pasar tiempo con mi mascota, que es $mascota. Actualmente me encuentro $estado_laboral.";
+echo "Hola, mi nombre es $nombre y tengo $edad años. 
+      Originalmente soy de $pais, específicamente del $ciudad, 
+      donde me desempeño como $profesion. 
+      Cuento con $años_estudio años estudiando la carrera y 
+      mi lenguaje de programación favorito es $lenguaje_favorito. 
+      En mis tiempos libres disfruto mucho de $hobby y de pasar 
+      tiempo con mi mascota, que es $mascota. 
+      Actualmente me encuentro $estado_laboral.";
 ?>
